edad93497f716b49cb4b060bb9e04b8db9cec32e
bharatgudihal <bharat.gudi@gmail.com>
2014-02-17 00:32:45 +0530

    made changes for http server
    done all changes required for c sharp

---
+++ lyncServer/Program.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Lync.Model;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Collections;
+namespace ConsoleApplication1
+{
+
+
+    class Person
+    {
+        public  string empid;
+        public  string SipiD;
+        public  string email;
+        public  string password;
+
+        public static Hashtable table;
+        public static void  setHashMap()
+        {
+            table = new Hashtable();
+
+        }
+        public static void add(string empid, Person p)
+        {
+            table.Add(empid, p);
+        }
+        public static Person get(String empid)
+        {
+            return (Person)table[empid];
+        }
+    }
+    class Program
+    {
+        private LyncClient lyncClient;
+        static void Main(string[] args)
+        {
+
+
+            Person p = new Person();
+            //set parameters here 
+            p.empid = "";
+            p.email = "";
+            p.SipiD = "";
+            p.password = "";
+
+
+            Program.startWebserver(p);
+
+            
+
+            //Program p = new Program();
+
+           
+
+            //p.connect("hello india");
+
+        }
+
+        public static void startWebserver(Person p)
+        {
+
+            // hit url like  http://localhost:8089/test?id=6759&status=hello
+
+            http_server ws = new http_server(SendResponse, "http://localhost:8089/test/");
+            ws.setPersonStack(p);
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
+        }
+        public  void connect(Person p,string status)
+        {
+            try
+            {
+                //"sip:abhijit.roy@comviva.com", "abhijit.roy@comviva.com", ""
+
+
+                lyncClient = LyncClient.GetClient();
+                //lyncClient.SignInConfiguration.InternalServerUrl = "comviva.com";
+               
+                if(lyncClient.State == ClientState.SignedOut)
+                lyncClient.BeginSignIn(p.SipiD, p.email, p.password, SignInCallback, null);
+                Console.WriteLine("please wait we are signing in");
+                while (lyncClient.State == ClientState.SigningIn)
+                {
+                        
+                }
+
+                if (lyncClient.State == ClientState.SignedIn)
+                {
+                    Console.WriteLine("client has signed in");
+                    setnote(status);
+                    Console.WriteLine("status updated");
+
+
+                    object[] asyncState = { lyncClient, "" };
+                    lyncClient.BeginSignOut(SignOutCallback, asyncState);
+
+                }
+
+                
+
+
+            }
+            catch (ClientNotFoundException clientNotFoundException)
+            {
+                Console.WriteLine(clientNotFoundException);
+                return;
+            }
+            catch (NotStartedByUserException notStartedByUserException)
+            {
+                Console.Out.WriteLine(notStartedByUserException);
+                return;
+            }
+            catch (LyncClientException lyncClientException)
+            {
+                Console.Out.WriteLine(lyncClientException);
+                return;
+            }
+            catch (SystemException systemException)
+            {
+                if (IsLyncException(systemException))
+                {
+                    // Log the exception thrown by the Lync Model API.
+                    Console.WriteLine("Error: " + systemException);
+                    return;
+                }
+                else
+                {
+                    // Rethrow the SystemException which did not come from the Lync Model API.
+                    throw;
+                }
+            }
+
+           
+
+        }
+        private void SignOutCallback(IAsyncResult result)
+        {
+            try
+            {
+                lyncClient.EndSignOut(result);
+            }
+            catch (LyncClientException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (SystemException systemException)
+            {
+                if (IsLyncException(systemException))
+                {
+                    // Log the exception thrown by the Lync Model API.
+                    Console.WriteLine("Error: " + systemException);
+                }
+                else
+                {
+                    // Rethrow the SystemException which did not come from the Lync Model API.
+                    throw;
+                }
+            }
+        }
+        private void SignInCallback(IAsyncResult result)
+        {
+            try
+            {
+                lyncClient.EndSignIn(result);
+            }
+            catch (LyncClientException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (SystemException systemException)
+            {
+                if (IsLyncException(systemException))
+                {
+                    // Log the exception thrown by the Lync Model API.
+                    Console.WriteLine("Error: " + systemException);
+                }
+                else
+                {
+                    // Rethrow the SystemException which did not come from the Lync Model API.
+                    throw;
+                }
+            }
+
+        }
+        private bool IsLyncException(SystemException ex)
+        {
+            return
+                ex is NotImplementedException ||
+                ex is ArgumentException ||
+                ex is NullReferenceException ||
+                ex is NotSupportedException ||
+                ex is ArgumentOutOfRangeException ||
+                ex is IndexOutOfRangeException ||
+                ex is InvalidOperationException ||
+                ex is TypeLoadException ||
+                ex is TypeInitializationException ||
+                ex is InvalidComObjectException ||
+                ex is InvalidCastException;
+        }
+        private void setnote(string status)
+        {
+            //Add the personal note to the contact information items to be published
+            Dictionary<PublishableContactInformationType, object> newInformation =
+                new Dictionary<PublishableContactInformationType, object>();
+            newInformation.Add(PublishableContactInformationType.PersonalNote, status);
+
+            //Publish the new personal note value
+            try
+            {
+                lyncClient.Self.BeginPublishContactInformation(newInformation, PublishContactInformationCallback, null);
+            }
+            catch (LyncClientException lyncClientException)
+            {
+                Console.WriteLine(lyncClientException);
+            }
+            catch (SystemException systemException)
+            {
+                if (IsLyncException(systemException))
+                {
+                    // Log the exception thrown by the Lync Model API.
+                    Console.WriteLine("Error: " + systemException);
+                }
+                else
+                {
+                    // Rethrow the SystemException which did not come from the Lync Model API.
+                    throw;
+                }
+            }
+        }
+        private void PublishContactInformationCallback(IAsyncResult result)
+        {
+            lyncClient.Self.EndPublishContactInformation(result);
+        }
+
+    }
+
+}
---
+++ lyncServer/http_server.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class http_server
+    {
+
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _responderMethod;
+        public Person pstack;
+
+        public void setPersonStack(Person p)
+        {
+            pstack = p;
+        }
+        public http_server(string[] prefixes, Func<HttpListenerRequest, string> method)
+        {
+
+            
+
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException(
+                    "Needs Windows XP SP2, Server 2003 or later.");
+ 
+            // URI prefixes are required, for example 
+            // "http://localhost:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+ 
+            // A responder method is required
+            if (method == null)
+                throw new ArgumentException("method");
+ 
+            foreach (string s in prefixes)
+                _listener.Prefixes.Add(s);
+ 
+            _responderMethod = method;
+            _listener.Start();
+        }
+
+        public http_server(Func<HttpListenerRequest, string> method, params string[] prefixes)
+            : this(prefixes, method) { }
+ 
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((c) =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                string rstr = _responderMethod(ctx.Request);
+                                byte[] buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+
+                                HttpListenerRequest request = ctx.Request;
+                                 ShowRequestData(request);
+                                 ShowRequestProperties1(request);
+                            }
+                            catch { } // suppress any exceptions
+                            finally
+                            {
+                                // always close the stream
+                                ctx.Response.OutputStream.Close();
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch { } // suppress any exceptions
+            });
+        }
+        
+
+        public  void ShowRequestProperties1(HttpListenerRequest request)
+        {
+            // Display the MIME types that can be used in the response. 
+            string[] types = request.AcceptTypes;
+            if (types != null)
+            {
+                Console.WriteLine("Acceptable MIME types:");
+                foreach (string s in types)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            // Display the language preferences for the response.
+            types = request.UserLanguages;
+            if (types != null)
+            {
+                Console.WriteLine("Acceptable natural languages:");
+                foreach (string l in types)
+                {
+                    Console.WriteLine(l);
+                }
+            }
+            String status, empid;
+            // Display the URL used by the client.
+            Console.WriteLine("URL: {0}", request.Url.OriginalString);
+            Console.WriteLine("Raw URL: {0}", request.RawUrl);
+            Console.WriteLine("emp id" + request.QueryString["id"]);
+           Console.WriteLine("status"+ request.QueryString["status"]);
+          //  Console.WriteLine("status" + request.QueryString["id"]);
+
+           empid = request.QueryString["id"];
+           status = request.QueryString["status"];
+           Person emp = Person.get(empid);
+           Program prg = new Program();
+            prg.connect(emp ,status); 
+
+
+           // var queryString = HttpUtility.ParseQueryString(request.Url.Query);
+            // Display the referring URI.
+            Console.WriteLine("Referred by: {0}", request.UrlReferrer);
+
+            //Display the HTTP method.
+            Console.WriteLine("HTTP Method: {0}", request.HttpMethod);
+            //Display the host information specified by the client;
+            Console.WriteLine("Host name: {0}", request.UserHostName);
+            Console.WriteLine("Host address: {0}", request.UserHostAddress);
+            Console.WriteLine("User agent: {0}", request.UserAgent);
+        }
+        public  void ShowRequestData(HttpListenerRequest request)
+        {
+            if (!request.HasEntityBody)
+            {
+                Console.WriteLine("No client data was sent with the request.");
+               // return;
+            }
+            System.IO.Stream body = request.InputStream;
+            System.Text.Encoding encoding = request.ContentEncoding;
+            System.IO.StreamReader reader = new System.IO.StreamReader(body, encoding);
+            if (request.ContentType != null)
+            {
+                Console.WriteLine("Client data content type {0}", request.ContentType);
+            }
+            Console.WriteLine("Client data content length {0}", request.ContentLength64);
+
+            Console.WriteLine("Start of client data:");
+            // Convert the data to a string and display it on the console. 
+            string s = reader.ReadToEnd();
+            Console.WriteLine(s);
+            Console.WriteLine("End of client data:");
+            body.Close();
+            reader.Close();
+            // If you are finished with the request, it should be closed also.
+        }
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+
+
+
+ 
+    
+
+    }
+
+
+    
+}
